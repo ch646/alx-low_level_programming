64a397a1898b6b248897167e4383e7d6393b05d4
christabel <christabelwambui678@gmail.com>
2023-04-20 12:32:12 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main-start point
+ * main-Start point
  *
  * return:always 0 (correct)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
