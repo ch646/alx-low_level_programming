4a315344b820a2cb7f49080122eb8981810d3191
christabel <christabelwambui678@gmail.com>
2023-04-24 03:59:37 -0700

    last digitif loop in string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,16 +14,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (Last digit of n >0 )
+	if (Last(digit of n > 0 ))
 	{
-		printf("%s and is greater than 5\n", n);
+		printf("%d and is greater than 5\n", n);
 	}
-	else if (Last digit of n == 0)
+	else if (Last(digit of n == 0))
 	{
-		printf("%s and is 0\n", n)
+		printf("%d and is 0\n", n)
 	}
 	else
 	{
-		printf("%s and is less than 6 and not 0\n", n)
+		printf("%d and is less than 6 and not 0\n", n)
 	return (0);
 }
