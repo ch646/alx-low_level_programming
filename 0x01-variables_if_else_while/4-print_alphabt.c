6aff7a0e6db666e78a14ec08d5d3ca91f8134e4a
christabel <christabelwambui678@gmail.com>
2023-04-24 11:04:50 -0700

    letters except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+int maain(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; ++x)
+	{
+		if (x != 'q' && x != 'e')
+	}
+	{
+	putchar(x);
+}
+	putchar("\n);
+	return (0);
+}
