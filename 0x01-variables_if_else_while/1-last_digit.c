bddbfdc9705d264658ac87f568761bd352d80e4b
christabel <christabelwambui678@gmail.com>
2023-04-24 06:45:41 -0700

    last digitif loop in string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include<stdio.h>
 
 /**
- *main-prints the last digit of a random number and if it is greater than 5,less than 6 or 0.
+ *main-prints the last digit of a random number 
+ *	and if it is greater than 5,less than 6 or 0.
  *
  *Return: Always 0 (success)
  */
