ce5ed665c022fb8449a0e8c963431d5761b87a81
christabel <christabelwambui678@gmail.com>
2023-04-24 11:43:19 -0700

    letters except

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,19 @@
 #include<stdio.h>
 
 /**
+ *main-prints all letters in lowercase except q and e.
  *
- *
- *
+ *Return: Always 0 (success)
  */
 int maain(void)
 {
 	char x;
 
 	for (x = 'a'; x <= 'z'; ++x)
-	{
-		if (x != 'q' && x != 'e')
-	}
-	{
+		if (x != 'q' && x != 'e')}
+{
 	putchar(x);
 }
-	putchar("\n);
+	putchar("\n");
 	return (0);
 }
