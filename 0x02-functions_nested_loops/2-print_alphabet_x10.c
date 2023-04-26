2901fd48844193919cec2cec068da7a25ef9f82c
christabel <christabelwambui678@gmail.com>
2023-04-26 04:08:36 -0700

    prints 10 times alphabets in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,8 @@
 void print_alphabet() {
     int i, j;
     for (i = 0; i < 10; i++) {
-        for (j = 0; j < 26; j++) {
+        for (j = 0; j < 26; j++) 
+	{
             _putchar('a' + j);
         }
         _putchar('\n');
