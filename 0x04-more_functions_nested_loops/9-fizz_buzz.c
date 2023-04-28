8d8b2c29daaa4f7642d9e3e7c97df60a93e72899
christabel <christabelwambui678@gmail.com>
2023-04-28 15:41:08 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 #include <stdio.h>
@@ -19,7 +18,7 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-	{
+
 	if (i % 3 == 0 && i % 5 != 0)
 	{
 	printf(" Fizz");
@@ -42,6 +41,7 @@ int main(void)
 	}
 	{
 	printf("\n");
-
+	}
 	return (0);
+
 }
