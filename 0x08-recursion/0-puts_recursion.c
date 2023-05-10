cc5f5cd572779d56327b47ed2d2a78c809235748
christabel <christabelwambui678@gmail.com>
2023-05-10 08:31:35 -0700

    recursion print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,20 @@
 
 /**
  * _puts_recursion- prints a string followed by a new line
- * @s: char
+ * @s: input value
  *
  * Return: 0 Always a success
  */
 void _puts_recursion(char *s)
-{
-	if 
-	(*s)
-
+{ 
+	if (*s)
+	{
 	_putchar(*s);
 	_puts_recursion(s + 1);
-	
+	}
 	else
-	_putchar('\0');
+
+	{
+	_putchar ('\n');
+	}
 }
